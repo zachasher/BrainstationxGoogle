d5553deb8b9af115ad30ead0fda6e900aca2a462
Zach Asher <zach.asher@hotmail.co.uk>
2023-07-21 22:36:06 +0300

    Latest commit

--- src/Pages/Components/Content.js
+++ src/Pages/Components/Content.js
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import image2 from "../../Assets/images/image2.png";
 import { Paper } from "@mui/material";
 import { WidthFull } from "@mui/icons-material";
+import "./hub-info.css";
 
 const bull = (
   <Box
@@ -25,22 +26,23 @@ export default function BasicCard() {
         <Typography variant="h5" component="div" sx={{pt: 1, pb: 1}}>
           AI is revolutionising the way <br /> we stay fit & healthy
         </Typography>
-        <Typography variant="body2">
+        <Typography variant="body2" sx={{pb: 2}}>
           Artificial intelligence (AI) is rapidly transforming the health and
           fitness industry. AI-powered apps and devices are now able to track
           our progress, provide personalized feedback, and even create custom
           workouts for us. This personalized approach to fitness is helping
           people reach their goals faster and more effectively.
-          <br />
-          {'"a benevolent smile"'}
         </Typography>
-        <Box variant="outlined" width="1" height="300px"  objectFit="contain" sx={{ pt: 2 }}>
-          <img src={image2}/>
-        </Box>
+        <Box sx={{width: 1}}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    //   backgroundImage="url('../../Assets/images/image2.png')"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover">
+      <img src={image2} className="sports-image"/>
+    </Box>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
--- src/Pages/Components/HubInfo.js
+++ src/Pages/Components/HubInfo.js
@@ -8,6 +8,9 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Box from "@mui/material/Box";
 import { grey } from '@mui/material/colors';
+import graph from "../../Assets/images/Screen Shot 2023-07-21 at 3.25.33 PM.png";
+import "./hub-info.css";
+
 
 function HubInfo() {
   return (
@@ -15,11 +18,8 @@ function HubInfo() {
     justifyContent="center"
     alignItems="center">
       <Card sx={{ width: 1 }}>
-      <Typography variant="h5" component="h2">
+      <Typography variant="h5" component="h2" sx={{p: 2}}>
         Ways in which AI can improve your health
-      </Typography>
-      <Typography variant="body1" component="h2" sx={{color:"grey"}}>
-        Learn everything you need to about AI
       </Typography>
         <Accordion>
           <AccordionSummary
@@ -27,13 +27,11 @@ function HubInfo() {
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography sx={{ color: "#4285F4" }}>1. How AI can improve your health</Typography>
+            <Typography sx={{ color: "#4285F4" }}>1. Personalized health and fitness plans</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+            AI can be used to analyze individual health data, such as fitness levels, medical history, and dietary habits, to create personalized health and fitness plans. These plans can be tailored to each individual's specific needs and goals, and can help people to achieve their health and fitness goals more effectively.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -43,13 +41,26 @@ function HubInfo() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography sx={{ color: "#4285F4" }}>2.</Typography>
+            <Typography sx={{ color: "#4285F4" }}>2. Real-time health monitoring</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+            AI-powered devices can be used to monitor biometric data, such as heart rate, blood pressure, and blood sugar levels, in real time. This data can be used to track progress, identify potential health problems, and provide personalized feedback to help people stay healthy.
+            </Typography>
+            <img src={graph} className="graph-image"/>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "#4285F4" }} />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography sx={{ color: "#4285F4" }}>3. Virtual coaching</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+            AI-powered personal trainers can provide virtual coaching to help people achieve their health and fitness goals. These trainers can use AI to analyze individual progress, provide personalized feedback, and adjust workout plans as needed.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -59,13 +70,11 @@ function HubInfo() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography sx={{ color: "#4285F4" }}>3.</Typography>
+            <Typography sx={{ color: "#4285F4" }}>4. Gamification</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+            AI can be used to gamify health and fitness apps and programs. This can make them more engaging and fun, which can help people to stay motivated and reach their goals.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -75,29 +84,13 @@ function HubInfo() {
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography sx={{ color: "#4285F4" }}>4.</Typography>
+            <Typography sx={{ color: "#4285F4" }}>5. Social support</Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon sx={{ color: "#4285F4" }} />}
-            aria-controls="panel2a-content"
-            id="panel2a-header"
-          >
-            <Typography sx={{ color: "#4285F4" }}>5.</Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
+            AI can be used to create social support networks for people who are trying to improve their health and fitness. These networks can provide people with encouragement, motivation, and tips, which can help them to stay on track.
+
+These are just a few of the ways that AI can improve health and fitness. As AI technology continues to develop, we can expect to see even more innovative and effective ways to use AI to improve our health and well-being.
             </Typography>
           </AccordionDetails>
         </Accordion>
